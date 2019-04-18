b176832ab9dc2faade52214854b4e53a6fa1ef65
Nathanael Liechti <liechtinat@ict-campus.net>
2019-04-18 16:06:18 +0200

    Worked on explainScene

--- src/net/ictcampus/fangis/GameFieldGUI.java
+++ src/net/ictcampus/fangis/GameFieldGUI.java
@@ -12,11 +12,18 @@ import javafx.stage.Stage;
 
 public class GameFieldGUI extends Application implements EventHandler<ActionEvent> {
 
-    //Instancevariabels
+    //Instancevariabels for all Scenes
     private Stage primarystage;
     private Scene setNameScene, explainScene, welcomeScene;
     private BorderPane welcomeScenePane;
     private GridPane setNameScenePane, explainScenePane;
+
+    //Instancevariabels for explainScene
+    private Label explainTitle, catcherExplanation, escaperExplanation;
+    private Button gameStart;
+
+
+    //Instancevariables for welcomeScene
     private Label welcomeText, gameTitle;
     private Button playButton;
 
@@ -36,6 +43,11 @@ public class GameFieldGUI extends Application implements EventHandler<ActionEven
         explainScene = new Scene(explainScenePane);
         setNameScene = new Scene(setNameScenePane);
 
+        primaryStage.setTitle("Testtile");
+        primaryStage.setScene(explainScene);
+        primaryStage.show();
+    }
+    public void buildWelcomeScene() {
         //Create Nodes for welcomeScene
         welcomeText = new Label("Welcome to our Labyrinth-Fanigs Game. This is a simple \nGame made by zauggmo and Technat314");
         gameTitle = new Label("Labyrinth-Fangis");
@@ -54,6 +66,28 @@ public class GameFieldGUI extends Application implements EventHandler<ActionEven
         gameTitle.getStyleClass().add("gameTitle");
         primaryStage.setScene(welcomeScene);
         primaryStage.show();
+    }
+
+    public void buildExplainScene() {
+
+        //Initialize Nodes for Grid
+        gameStart = new Button("Let's Go!");
+        explainTitle = new Label("How it Works...");
+        catcherExplanation = new Label("-Your aim is it catch the other Player by simply touching him \n- Controll with WASD \n-Throw Bananas with r");
+        escaperExplanation =  new Label("-Your aim is it to escape from the catcher until the Timer is done \n- Controll with Arrow-Keys \n- Throw Bananas with 1");
+
+        //Put Nodes on Grid
+        explainScenePane.add(explainTitle, 0,0);
+        GridPane.setColumnSpan(explainTitle, 2);
+        explainScenePane.add(catcherExplanation, 0, 1);
+        explainScenePane.add(escaperExplanation, 1,1);
+        explainScenePane.add(gameStart, 0,2);
+        GridPane.addColumnSpan(gameStart, 2);
+
+        //Some styling for Grid
+        explainScenePane.getStylesheets().add(getClass().getResource("explainScene.css").toExternalForm());
+        explainTitle.getStyleClass().add("explainTitle");
+
 
     }
 
