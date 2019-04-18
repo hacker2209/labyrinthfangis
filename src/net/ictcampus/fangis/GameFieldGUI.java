fa08aa7db0f2915683c97b4ed70d184694bcee8b
zauggmo <moritz.zaugg@ict-campus.net>
2019-04-18 09:53:23 +0200

    GameFielGUI erstellt

---
+++ src/net/ictcampus/fangis/GameFieldGUI.java
@@ -0,0 +1,16 @@
+package net.ictcampus.fangis;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameFieldGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
