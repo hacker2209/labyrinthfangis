39970aeb6e0636e1242e100d96e8350400a50417
zauggmo <moritz.zaugg@ict-campus.net>
2019-04-24 08:13:12 +0200

    player instanz variablen erstellt

--- src/net/ictcampus/fangis/Player.java
+++ src/net/ictcampus/fangis/Player.java
@@ -1,4 +1,31 @@
 package net.ictcampus.fangis;
 
 public class Player {
+    private String playerName;
+    private int bananaCount;
+    private String playerRoll;
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setBananaCount(int bananaCount) {
+        this.bananaCount = bananaCount;
+    }
+
+    public void setPlayerRoll(String playerRoll) {
+        this.playerRoll = playerRoll;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getBananaCount() {
+        return bananaCount;
+    }
+
+    public String getPlayerRoll() {
+        return playerRoll;
+    }
 }
