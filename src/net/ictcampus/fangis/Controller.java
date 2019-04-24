9b90b391ea90524b59c15ac89ab6eb75df275018
Nathanael Liechti <liechtinat@ict-campus.net>
2019-04-24 16:39:40 +0200

    Working version with Logical in controller !

--- src/net/ictcampus/fangis/Controller.java
+++ src/net/ictcampus/fangis/Controller.java
@@ -1,37 +1,124 @@
 package net.ictcampus.fangis;
 
 import javafx.animation.Animation;
-import javafx.animation.AnimationTimer;
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.shape.Box;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Controller extends Application {
+public class Controller extends Application implements EventHandler<ActionEvent> { //KeyListener {
+
+    private Button playButton, nextButton, abrButton, gameStart;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void start(Stage primarystage) {
-        GameFieldGUI gui = new GameFieldGUI(primarystage, this);
-        gui.buildWelcomeScene();
+    private GameGui gui;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        playButton = new Button("Play");
+        gameStart = new Button("Let's Go!");
+        nextButton = new Button("Next");
+        abrButton = new Button("Abbrechen");
+        gui = new GameGui(primaryStage, playButton, nextButton, abrButton, gameStart);
+        gui.buildWelcomeScreen();
+        playButton.setOnAction(this);
+        nextButton.setOnAction(this);
+        gameStart.setOnAction(this);
+    }
 
-        AnimationTimer ani = new AnimationTimer(){
 
-            @Override
-            public void handle(long arg0) {
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == playButton) {
+            gui.buildSetNameScreen();
+        }
+        else if (event.getSource() == nextButton) {
+            if (!gui.txtPlayer1.getText().equals("") && !gui.txtPlayer2.getText().equals("")) {
+                gui.buildExplainScreen();
+            }
+            else {
+                if (gui.lblErrorMessage.getScene() == null) {
+                    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.1), gui.lblErrorMessage);
+                    fadeTransition.setFromValue(1.0);
+                    fadeTransition.setToValue(0.0);
+                    fadeTransition.setCycleCount(Animation.INDEFINITE);
+                    fadeTransition.play();
+                    gui.setNameScenePane.add(gui.lblErrorMessage, 0,4);
+                    GridPane.setColumnSpan(gui.lblErrorMessage, 2);
+                }
+                else {
+                   gui.setNameScenePane.getChildren().remove(gui.lblErrorMessage);
+                    GridPane.setColumnSpan(gui.lblErrorMessage, 2);
+                }
 
             }
-        };
-        ani.start();
+        }
+        else if (event.getSource() == gameStart) {
+            gui.buildGameFieldScreen();
+        }
     }
+
+
+
+
     //Handels all events
 
-
+//    //Hanlde Methode für Buttonactions
+//    public void handle(ActionEvent event) {
+//        if (event.getSource() == playButton) {
+//            buildSetNameScene();
+//        }
+//        else if (event.getSource() == nextButton) {
+//            if (!txtPlayer1.getText().equals("") && !txtPlayer2.getText().equals("")) {
+//                buildExplainScene();
+//            }
+//            else {
+//                if (lblErrorMessage.getScene() == null) {
+//                    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.1), lblErrorMessage);
+//                    fadeTransition.setFromValue(1.0);
+//                    fadeTransition.setToValue(0.0);
+//                    fadeTransition.setCycleCount(Animation.INDEFINITE);
+//                    fadeTransition.play();
+//                    setNameScenePane.add(lblErrorMessage, 0,4);
+//                    GridPane.setColumnSpan(lblErrorMessage, 2);
+//                }
+//                else {
+//                    setNameScenePane.getChildren().remove(lblErrorMessage);
+//                    setNameScenePane.add(lblErrorMessage, 0,4);
+//                    GridPane.setColumnSpan(lblErrorMessage, 2);
+//                }
+//
+//            }
+//        }
+//        else if (event.getSource() == gameStart) {
+//            buildGameField();
+//        }
+//    }
+//
+//    @Override
+//    public void keyTyped(KeyEvent e) {
+//        switch(e.getKeyCode()) {
+//            case KeyEvent.VK_B:
+//                animator.stop();
+//        }
+//    }
+//
+//    @Override
+//    public void keyPressed(KeyEvent e) {
+//
+//    }
+//
+//    @Override
+//    public void keyReleased(KeyEvent e) {
+//
+//    }
 }
---
+++ src/net/ictcampus/fangis/GameGui.java
@@ -0,0 +1,168 @@
+package net.ictcampus.fangis;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class GameGui {
+
+    //Instancevariabels for all Scenes
+    protected Stage primarystage;
+    protected Scene setNameScene, explainScene, welcomeScene, gameScene;
+    protected BorderPane welcomeScenePane;
+    protected GridPane setNameScenePane, explainScenePane, gameRasterPane;
+    protected Pane gameFieldPane;
+
+    //Instancevariabels for explainScene
+    protected Label explainTitle, catcherExplanation, escaperExplanation, catchername, escapername;
+    protected Button gameStart;
+
+    //Instancevariables for welcomeScene
+    protected Label welcomeText, gameTitle;
+    protected Button playButton;
+
+    //Instancevariabels for setNameScene
+    protected Button nextButton;
+    protected Label lblName, lblPlayer1, lblPlayer2, lblErrorMessage, lblNothing;
+    protected TextField txtPlayer1, txtPlayer2;
+
+    //Instancevariabels for gameField
+    protected Label lblScore, lblTimer;
+    protected Button abrButton;
+
+    public GameGui(Stage primarystage, Button playButton, Button nextButton, Button abrButton, Button gameStart) {
+        this.primarystage = primarystage;
+        this.playButton = playButton;
+        this.nextButton = nextButton;
+        this.abrButton = abrButton;
+        this.gameStart = gameStart;
+    }
+
+    public void buildWelcomeScreen() {
+        welcomeScenePane = new BorderPane();
+        welcomeScene = new Scene(welcomeScenePane, 300, 160);
+        //Create Nodes for welcomeScene
+        welcomeText = new Label("Welcome to our Labyrinth-Fanigs Game!\nThis is a simple Game\nmade by zauggmo and Technat314");
+        gameTitle = new Label("Labyrinth-Fangis");
+
+        //Put Nodes on PaneT
+        welcomeScenePane.setTop(gameTitle);
+        welcomeScenePane.setBottom(playButton);
+        welcomeScenePane.setCenter(welcomeText);
+        //Add some Style to welcomeScene
+        welcomeScene.getStylesheets().add(getClass().getResource("welcomeScene.css").toExternalForm());
+        gameTitle.getStyleClass().add("gameTitle");
+        playButton.getStyleClass().add("playButton");
+        welcomeScenePane.getStyleClass().add("pane");
+        welcomeText.getStyleClass().add("text");
+        //Show welcomeScene
+        primarystage.setScene(welcomeScene);
+        primarystage.show();
+    }
+
+
+
+    //    public void buildWelcomeScene() {
+//    }
+    public void buildExplainScreen() {
+        explainScenePane = new GridPane();
+        explainScene = new Scene(explainScenePane, 400, 300);
+        //Initialize Nodes for Grid
+        catchername  = new Label(txtPlayer1.getText());
+        escapername = new Label(txtPlayer2.getText());
+        explainTitle = new Label("How it Works...");
+        catcherExplanation = new Label("- Your aim is\n to catch\nthe other by touching him\n- Controll with WASD \n- Throw Bananas with r");
+        escaperExplanation =  new Label("- Your aim is\n to escape  \n- Controll with Arrow-Keys \n- Throw Bananas with 1");
+        GridPane.setColumnSpan(explainTitle,2);
+        //Put Nodes on Grid
+        explainScenePane.add(explainTitle, 0,0);
+        explainScenePane.add(catchername, 0, 1);
+        explainScenePane.add(escapername, 1, 1);
+        explainScenePane.add(catcherExplanation, 0, 2);
+        explainScenePane.add(escaperExplanation, 1,2);
+        explainScenePane.add(gameStart, 0,3);
+        GridPane.setColumnSpan(gameStart, 2);
+        //Some styling for Grid
+        explainScenePane.getStylesheets().add(getClass().getResource("explainScene.css").toExternalForm());
+        explainTitle.getStyleClass().add("explainTitle");
+        explainScenePane.getStyleClass().add("pane");
+        catcherExplanation.getStyleClass().add("explan");
+        escaperExplanation.getStyleClass().add("explan");
+        catchername.getStyleClass().add("name");
+        escapername.getStyleClass().add("name");
+        gameStart.getStyleClass().add("start");
+        explainScenePane.setVgap(30);
+        explainScenePane.setHgap(30);
+        //Show explainScene
+        primarystage.setScene(explainScene);
+        primarystage.show();
+    }
+
+    public void buildSetNameScreen() {
+        setNameScenePane = new GridPane();
+        setNameScene = new Scene(setNameScenePane, 300, 200);
+        //Initialize Nodes for Grid
+        lblName = new Label("Set your Names");
+        lblPlayer1 = new Label("Player 1: ");
+        lblPlayer2 = new Label("Player 2: ");
+        txtPlayer1 = new TextField();
+        txtPlayer2 = new TextField();
+        lblErrorMessage = new Label("No valid Names!");
+        lblNothing = new Label();
+        //Grid styling
+        setNameScene.getStylesheets().add(getClass().getResource("setNameScene.css").toExternalForm());
+        lblName.getStyleClass().add("nameTitle");
+        setNameScenePane.getStyleClass().add("pane");
+        nextButton.getStyleClass().add("button");
+        lblPlayer1.getStyleClass().add("player");
+        lblPlayer2.getStyleClass().add("player");
+        lblErrorMessage.getStyleClass().add("error");
+        setNameScenePane.setVgap(10);
+        setNameScenePane.setHgap(30);
+        //Put Nodes into Grid
+        setNameScenePane.add(lblName, 0,0);
+        setNameScenePane.setColumnSpan(lblName, 2);
+        setNameScenePane.add(nextButton,0,3);
+        setNameScenePane.setColumnSpan(nextButton, 2);
+        setNameScenePane.add(lblPlayer1, 0, 1);
+        setNameScenePane.add(txtPlayer1, 1, 1);
+        setNameScenePane.add(lblPlayer2, 0, 2);
+        setNameScenePane.add(txtPlayer2, 1, 2);
+        //Show Scene
+        primarystage.setScene(setNameScene);
+        primarystage.show();
+    }
+
+    public void buildGameFieldScreen() {
+        gameRasterPane = new GridPane();
+        gameFieldPane = new Pane();
+        gameScene = new Scene(gameRasterPane, 1000,600);
+        gameFieldPane.setMaxWidth(1000.0);
+        gameFieldPane.setMinHeight(400.0);
+        //Initialize Nodes for Grid
+        lblScore = new Label("The Score");
+        lblTimer = new Label("3:00");
+        //Grid Styling
+        setNameScene.getStylesheets().add(getClass().getResource("gameField.css").toExternalForm());
+        //Put Nodes on Raster
+        gameRasterPane.add(gameFieldPane,0,0);
+        gameRasterPane.add(abrButton,0,1);
+        gameRasterPane.add(lblScore,1,1);
+        gameRasterPane.add(lblTimer, 2,1);
+        //Make Players
+        Player catcher = new Player(0,0,40,40,lblPlayer1.getText(),"catcher", Color.RED);
+        Player escaper = new Player(((int)gameFieldPane.getMaxWidth() - 40), 0,40,40,lblPlayer2.getText(), "escaper", Color.BLUE);
+        gameFieldPane.getChildren().add(escaper);
+        gameFieldPane.getChildren().add(catcher);
+        //Show Scene
+        primarystage.setScene(gameScene);
+        primarystage.show();
+    }
+}
+
