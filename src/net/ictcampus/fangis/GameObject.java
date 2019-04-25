78589e96e9d2f8f455e88651a02868b6d68eba12
zauggmo <moritz.zaugg@ict-campus.net>
2019-04-25 11:44:50 +0200

    Game OBJECT

--- src/net/ictcampus/fangis/GameObject.java
+++ src/net/ictcampus/fangis/GameObject.java
@@ -1,4 +1,22 @@
 package net.ictcampus.fangis;
+import java.util.Random;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 
-public class GameObject {
+public class GameObject extends Rectangle{
+
+    public GameObject(int width, int height, Color color) {
+        super(width, height, color);
+
+    }
+
+    public void  randomPosition{
+        Random rand = new Random();
+        int x = rand.nextInt(900);
+        int y = rand.nextInt(500);
+        setTranslateX(x);
+        setTranslateY(y);
+    }
+
+
 }
