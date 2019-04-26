c574fa5f239aaf5949fdedc643ff008abf52371d
Nathanael Liechti <liechtinat@ict-campus.net>
2019-04-26 14:22:02 +0200

    Working on gameObjects

--- src/net/ictcampus/fangis/GameObject.java
+++ src/net/ictcampus/fangis/GameObject.java
@@ -1,19 +1,16 @@
 package net.ictcampus.fangis;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 
 public class GameObject extends Rectangle{
 
-    public GameObject(int width, int height, Color color) {
-        super(width, height, color);
 
-    }
-
-    public void  randomPosition(){
-        Random rand = new Random();
-        int x = rand.nextInt(900);
-        int y = rand.nextInt(500);
+    public GameObject(int x, int y,int w, int h, Color color) {
+        super(w,h,color);
         setTranslateX(x);
         setTranslateY(y);
     }
---
+++ src/net/ictcampus/fangis/checkCollision.java
@@ -0,0 +1,51 @@
+package net.ictcampus.fangis;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Platform;
+import javafx.scene.Group;
+
+public class checkCollision extends Thread {
+
+    private Controller con;
+    private GameGui gui;
+    private boolean done = false;
+    private AnimationTimer animationTimer;
+
+    public checkCollision(Controller con, GameGui gui) {
+        this.con = con;
+        this.gui = gui;
+    }
+
+    public void run() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                animationTimer = new AnimationTimer() {
+                    @Override
+                    public void handle(long now) {
+                        int caty = (int) con.catcher.getTranslateY();
+                        int catx = (int) con.catcher.getTranslateX();
+                        int escy = (int) con.escaper.getTranslateY();
+                        int escx = (int) con.escaper.getTranslateX();
+                        gui.lblTimer.setText("Catcher Y: " + caty + "Catcher X: " + catx + "Escaper Y: " + escy + "Escaper X: " + escx);
+                        if (con.catcher.getBoundsInParent().intersects(con.escaper.getBoundsInParent()))  {
+                            terminate();
+                        }
+                    }
+                };
+                animationTimer.start();
+
+            }
+        });
+    }
+
+
+
+    private void terminate() {
+        con.escaper.catched();
+        con.ani.stop();
+        animationTimer.stop();
+        gui.buildGameOverScreen();
+
+    }
+}
