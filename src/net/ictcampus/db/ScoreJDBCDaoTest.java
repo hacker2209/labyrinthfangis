be898fc6013be40b9a5cfbea591a5ea7ebdbf56c
zauggmo <moritz.zaugg@ict-campus.net>
2019-05-01 13:50:30 +0200

    JUnit

---
+++ src/net/ictcampus/db/ScoreJDBCDaoTest.java
@@ -0,0 +1,20 @@
+package net.ictcampus.db;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreJDBCDaoTest {
+
+    @Test
+    public void insertScore() {
+    }
+
+    @Test
+    public void maxScore() {
+    }
+
+    @Test
+    public void allUsername() {
+    }
+}
---
+++ src/net/ictcampus/fangis/gameObejctGeneratorTest.java
@@ -0,0 +1,25 @@
+package net.ictcampus.fangis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class gameObejctGeneratorTest {
+
+    @Test
+    public void randomXPosition() {
+    }
+
+    @Test
+    public void randomYPosition() {
+    }
+
+    @Test
+    public void fillDimensions() {
+
+    }
+
+    @Test
+    public void getRandomdimensions() {
+    }
+}
